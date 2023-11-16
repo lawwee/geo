334953efd5e433bfa9d5944ff78841cb80dab0fc
Lawwee <lawalmohammed567@gmail.com>
2023-11-16 17:37:01 +0100

    created controller file for landmark

---
+++ controllers/landmark.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const Landmark = require("../models/landmark");
+
+const discover = async (req, res) => {
+    try {
+        const { long, lat, propertyType } = req.body;
+        
+        if (!long || lat) {
+            return res.status(400).json({
+                message: 'Longitude and Latitude are required parameters'
+            })
+        }
+
+        const query = {
+            location: {
+                $nearSphere: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: [parseFloat(long), parseFloat(lat)]
+                    },
+                    $maxDistance: 100000
+                }
+            }
+        }
+
+        const nearbyPlaces = await Landmark.find(query);
+
+        res.json({
+            message: "Retreieved successfully",
+            ...nearbyPlaces
+        })
+    } catch (error) {
+        console.error("error discovering nearby places", error.message);
+        res.status(500).json({
+            error: "Internal Server error"
+        })
+    }
+}
+
+module.exports = discover;
