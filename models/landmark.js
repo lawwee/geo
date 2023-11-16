0ee2e761dbf684d20499598d196b3423537d11e2
Lawwee <lawalmohammed567@gmail.com>
2023-11-16 16:49:21 +0100

    added model for landmark

---
+++ models/landmark.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const landmarkSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    landmarkType: {
+        type: String,
+        enum: ["Restaurant", "Hospital", "Gym", "School", "Mall"],
+        required: true
+    },
+    location: {
+        type: {
+            type: String,
+            default: "Point"
+        },
+        city: {
+            type: String, 
+            required: true
+        },
+        state: {
+            type: String,
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            index: '2dsphere'
+        }
+    }
+});
+
+landmarkSchema.index({ location: '2dsphere' });
+
+const Landmark = mongoose.model("Landmark", landmarkSchema);
+
+module.exports = Landmark;
